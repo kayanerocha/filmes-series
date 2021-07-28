33dad16f993680c43676c00cb9c5d32f829d7c15
kayanerocha <kayannerch@gmail.com>
2021-07-28 17:39:33 -0300

    subindo o projeto inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilmesSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
